01dae10eb9fdbd8337cd060b490f1490991e662b
sydneycollins <internet12>
2018-03-15 17:37:14 -0400

    Configuring Login

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var auth = require('.routes/auth');
+
+
+
 
 var app = express();
 
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,32 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+router.get('/login',function(req,res){
+    res.render('login',{
+        message:req.flash('loginMessage')})
+}
+
+
+router.get('/signup',function(req,res){
+    res.render('signup',{
+        message:req.flash('signupMessage')})
+})
+
+
+router.get('/logout',function(req,res){
+    req.logout();
+    res.redirect('/');
+}
+
+function isLoggedIn(req, res, next) {
+
+    // if user is authenticated in the session, carry on
+    if (req.isAuthenticated())
+        return next();
+
+    // if they aren't redirect them to the home page
+    res.redirect('/');
+}
+
 module.exports = router;
